3b9def16b87c895c0ec4bbaf11da93f36ca99dca
Mani Movassagh <mani.mgh@gmail.com>
2023-02-19 02:02:19 +0100

    add documents

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,37 +1,45 @@
-
-
 /**
- * @desc Get All Bootcamps
+ * @desc    Get All Bootcamps
+ * @route   GET /api/v1/bootcamps
+ * @access  Public
  */
-exports.getBootcamps=(req,res,next)=>{
-    res.status(200).json({success:true,"msg":`Get all Bootcamps !!`});
-
-}
-/**
- * @desc Get Bootcamp
- */
-exports.getBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,"msg":`Display bootcamp with id Number ${req.params.id}`});
+exports.getBootcamps = (req, res, next) => {
+    res.status(200).json({success: true, "msg": `Get all Bootcamps !!`});
 
 }
 /**
- * @desc Create Bootcamp
+ * @desc    Get Single Bootcamp
+ * @route   GET  /api/v1/bootcamps/:id
+ * @access  Public
  */
-exports.createBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,"msg":"create new bootcamp"});
-}
-/**
- * @desc Edit Bootcamp
- */
-exports.editBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,"msg":`Edit bootcamp with id Number ${req.params.id}`});
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, "msg": `Display bootcamp with id Number ${req.params.id}`});
 
 }
 /**
- * @desc Delete Bootcamp
+ * @desc     Create new Bootcamp
+ * @route    POST  /api/v1/bootcamps
+ * @access   Private
  */
-exports.deleteBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,"msg":`Delete bootcamp with id Number ${req.params.id}`});
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, "msg": "create new bootcamp"});
+}
+/**
+ * @desc     Update Bootcamp
+ * @route    PUT  /api/v1/bootcamps/:id
+ * @access   Private
+ */
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, "msg": `Edit bootcamp with id Number ${req.params.id}`});
+
+}
+/**
+ * @desc     Delete Bootcamp
+ * @route    DELETE  /api/v1/bootcamps/:id
+ * @access   Private
+ */
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, "msg": `Delete bootcamp with id Number ${req.params.id}`});
 
 }
 
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const {getBootcamps, getBootcamp, createBootcamp, editBootcamp, deleteBootcamp} = require("../controllers/bootcamps");
+const {getBootcamps, getBootcamp, createBootcamp, deleteBootcamp, updateBootcamp} = require("../controllers/bootcamps");
 const router = express.Router();
 
 router.get('/', getBootcamps)
 router.get('/:id', getBootcamp)
 router.post('/', createBootcamp)
-router.put('/:id', editBootcamp)
+router.put('/:id', updateBootcamp)
 router.delete('/:id', deleteBootcamp)
 
 module.exports = router;
