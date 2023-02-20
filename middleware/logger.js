0b3ebdefa561cac875ebce071c059f3f51217f34
Mani Movassagh <mani.mgh@gmail.com>
2023-02-20 17:11:58 +0100

    add docs

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,10 @@
+/**
+ * @desc     sample logger to show the middleware functionality
+ * @author   Mani Movassagh Ghazani
+ * @param req
+ * @param res
+ * @param next
+ */
 const logger=(req,res,next)=>{
     res.special='mani'
     console.log('Some logger to show on request')
