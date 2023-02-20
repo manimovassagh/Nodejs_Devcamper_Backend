9535dc8a019e6282d716cc781db554d1e38cc345
Mani Movassagh <mani.mgh@gmail.com>
2023-02-21 00:38:52 +0100

    color

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
-const {connect: connectInitializer} = require('mongoose')
-const mongoose = require('mongoose')
+const { connect: connectInitializer } = require("mongoose");
+const mongoose = require("mongoose");
 const connectDB = async () => {
-    mongoose.set('strictQuery', false)
-    const conn = await connectInitializer(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  mongoose.set("strictQuery", false);
+  const conn = await connectInitializer(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected: ${conn.connection.host}`.brightBlue);
 };
-
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,44 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan')
-const colors = require('colors')
-const connectDB = require('./config/db')
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+require("colors");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
 
 //Route files
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require("./routes/bootcamps");
 
 //init dotenv
-dotenv.config({path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
 //init the express app
 const app = express();
 
 //connect to MongoDB
- connectDB();
-
+connectDB();
 
 //body parser
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //apply middlewares
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
-
 const PORT = process.env.PORT || 4000;
 const server = app.listen(4000, () => {
-    console.log(`Server is up and running on port ${PORT}`.brightYellow)
+  console.log(`Server is up and running on port ${PORT}`.brightYellow);
 });
 
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error ${err.message}`.red)
-    server.close(() => {
-        process.exit(1);
-    })
-})
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error ${err.message}`.red);
+  server.close(() => {
+    process.exit(1);
+  });
+});
