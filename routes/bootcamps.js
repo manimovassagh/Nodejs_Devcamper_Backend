097de9af480690b3b52c305c46c519b936ea63da
Mani Movassagh <mani.mgh@gmail.com>
2023-02-21 12:07:02 +0100

    delete config

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const {getBootcamps, getBootcamp, createBootcamp, deleteBootcamp, updateBootcamp} = require("../controllers/bootcamps");
+const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  deleteBootcamp,
+  updateBootcamp,
+} = require("../controllers/bootcamps");
 const router = express.Router();
-
 
 /**
  * @description routes that need no parameters
  * @author Mani Movassagh
  */
-router.route('/')
-    .get(getBootcamps)
-    .post(createBootcamp)
+router.route("/").get(getBootcamps).post(createBootcamp);
 
 /**
  * @description routes with ID parameter
  * @author Mani Movassagh
  */
-router.route('/:id')
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 module.exports = router;
