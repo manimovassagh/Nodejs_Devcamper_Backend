01eed460f1fe7f996daae5618107688aa51607e1
Mani Movassagh <mani.mgh@gmail.com>
2023-02-22 19:29:21 +0100

    add locations

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify');
-
+const geocoder = require('../utils/geocoder')
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -110,9 +110,16 @@ const BootcampSchema = new mongoose.Schema(
 //create bootcamp slug from the name
 BootcampSchema.pre('save', function (next) {
 
-    this.slug = slugify(this.name,{lower:true})
+    this.slug = slugify(this.name, {lower: true})
     next();
 });
 //Geocode & create location field
-
+BootcampSchema.pre('save',async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location={
+        type:'Point',
+        coordinates:[loc[0].longitude,loc[0].latitude]
+    }
+    next()
+})
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
