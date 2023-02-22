e16240f9089702a750c6e0021ce176b89bdc20c3
Mani Movassagh <mani.mgh@gmail.com>
2023-02-22 22:18:08 +0100

    add seeder for auto import data

---
+++ seeder.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv')
+
+dotenv.config({ path: "./config/config.env" });
+
+
+const bootcamp = require('./models/Bootcamp')
+const {connect: connectInitializer} = require("mongoose");
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//Read Json File
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+
+//import Data
+const importData = async () => {
+    try {
+        await bootcamp.create(bootcamps)
+        console.log('Data imported ...'.brightGreen)
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Delete Data
+
+const deleteData = async () => {
+    try {
+        await bootcamp.deleteMany()
+        console.log('Data destroyed ...'.brightRed)
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+if (process.argv[2] === '-i') {
+    importData().catch((error) => {
+        console.log(error)
+    });
+} else if (process.argv[2] === '-d') {
+    deleteData().catch((error) => {
+        console.log(error)
+    });
+}
+
