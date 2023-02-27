a809fd46119be2a28a18963b5c594aeec73aaacf
Mani Movassagh <mani.mgh@gmail.com>
2023-02-27 16:00:25 +0100

    data imported

--- seeder.js
+++ seeder.js
@@ -1,54 +1,60 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const {connect,set} = require('mongoose');
-const colors = require('colors');
-const dotenv = require('dotenv')
+const { connect, set } = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config/config.env" });
 
-
-const bootcamp = require('./models/Bootcamp')
-const {connect: connectInitializer} = require("mongoose");
+const bootcamp = require("./models/Bootcamp");
+const course = require("./models/Course");
+const { connect: connectInitializer } = require("mongoose");
 
 set("strictQuery", false);
 connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Read Json File
-const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
 
 //import Data
 const importData = async () => {
-    try {
-        await bootcamp.create(bootcamps)
-        console.log('Data imported ...'.brightGreen.inverse)
-        process.exit();
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await bootcamp.create(bootcamps);
+    await course.create(courses);
+    console.log("Data imported ...".brightGreen.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //Delete Data
 
 const deleteData = async () => {
-    try {
-        await bootcamp.deleteMany()
-        console.log('Data destroyed ...'.brightRed.inverse)
-        process.exit();
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await bootcamp.deleteMany();
+    await course.deleteMany();
+    console.log("Data destroyed ...".brightRed.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-if (process.argv[2] === '-i') {
-    importData().catch((error) => {
-        console.log(error)
-    });
-} else if (process.argv[2] === '-d') {
-    deleteData().catch((error) => {
-        console.log(error)
-    });
+if (process.argv[2] === "-i") {
+  importData().catch((error) => {
+    console.log(error);
+  });
+} else if (process.argv[2] === "-d") {
+  deleteData().catch((error) => {
+    console.log(error);
+  });
 }
-
