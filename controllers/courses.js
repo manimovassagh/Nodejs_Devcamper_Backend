e31653b37734fc2ed437562f53dbf91b3096983a
Mani Movassagh <mani.mgh@gmail.com>
2023-02-27 18:03:15 +0100

    single course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,9 +1,33 @@
 const asyncHandler = require("../middleware/async");
 const Course = require("../models/Course");
+const ErrorResponse = require("../utils/errorResponse");
 
 /*
  * @desc    Get All courses
  * @route   GET /api/v1/courses
+ * @route   GET /api/v1/bootcamps/:bootcampId/courses
+ * @access  Public
+ */
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp",
+    select: "name description",
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+/*
+ * @desc    Get single courses
+ * @route   GET /api/v1/courses/:id
  * @route   GET /api/v1/bootcamps/:bootcampId/courses
  * @access  Public
  */
